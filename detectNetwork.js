1a4b80fdd5a7db7754e693dce1474a338ef5e672
Zachary <zinouye@gmail.com>
2017-12-17 02:12:18 -0800

    Updated files after some bugs

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,20 +8,23 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  var cardLength = String(cardNumber).length;
-  var prefix = cardNumber.slice(0, 2);
-  if (cardLength === 14 && (prefix === 38 || prefix === 39)) {
+  var cardLength = cardNumber.toString().length;
+      var prefix = cardNumber.toString().split('')[0];
+          prefix += cardNumber.toString().split('')[1]
+    //var prefix = Math.floor(cardNumber/(Math.exp(10, (cardLength-2))));
+  if (cardLength === 14 && (prefix === '38' || prefix === '39')) {
     return "Diner's Club";
   }
-  if (cardLength === 15 && (prefix === 34 || prefix === 37)) {
+  if (cardLength === 15 && (prefix === '34' || prefix === '37')) {
     return 'American Express';
   }
-  if (cardLength === 16 && (prefix === 51 || prefix === 52 || prefix === 53 || prefix === 54 || prefix === 55)) {
+  if (cardLength === 16 && (prefix === '51' || prefix === '52' || prefix === '53' || prefix === '54' || prefix === '55')) {
     return 'MasterCard';
   }
-  if (prefix === 4 && (cardLength === 13 || cardLength === 16 || cardLength === 19)) {
+  if (Math.floor(prefix/10) === 4 && (cardLength === 13 || cardLength === 16 || cardLength === 19)) {
     return 'Visa'
   }
+}
 
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
@@ -30,4 +33,3 @@ var detectNetwork = function(cardNumber) {
   // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-};
--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -17,9 +17,7 @@ describe('Introduction to Mocha Tests - READ ME FIRST', function() {
   // Once you've read and understood this section, please comment it out. 
   // You will not be able to proceed with a failing test. 
 
-  it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
-  });
+
 
   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
     // This test doesn't really test anything at all! It will pass no matter what.
@@ -45,7 +43,7 @@ describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
+    
  
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
