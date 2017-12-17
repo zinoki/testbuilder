2ebc4c7593f26e613e27ff75052212f08deb253b
Zachary <zinouye@gmail.com>
2017-12-16 21:30:05 -0800

    Made the detectNetwork function more specific for checking card prefix and card length

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,15 +8,26 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  if (cardNumber.length === 14) {
+  var cardLength = String(cardNumber).length;
+  var prefix = cardNumber.slice(0, 2);
+  if (cardLength === 14 && (prefix === 38 || prefix === 39)) {
     return "Diner's Club";
-  } else {
+  }
+  if (cardLength === 15 && (prefix === 34 || prefix === 37)) {
     return 'American Express';
+  }
+  if (cardLength === 16 && (prefix === 51 || prefix === 52 || prefix === 53 || prefix === 54 || prefix === 55)) {
+    return 'MasterCard';
+  }
+  if (prefix === 4 && (cardLength === 13 || cardLength === 16 || cardLength === 19)) {
+    return 'Visa'
   }
 
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
